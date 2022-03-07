be053e53733c28db7cc684c71a52946e86614f3a
kirby121 <83082132+kirby121@users.noreply.github.com>
2022-03-07 18:58:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const d = document,
+$trabajo1 = d.querySelector(".trabajo1")
+
+d.addEventListener("DOMContentLoaded", e =>{
+    $trabajo1.addEventListener("click", e =>{
+        window.open("https://www.canva.com/design/DAE6V_vZuFw/eRZBS5X3ezIIDcSBlHQbEA/view?utm_content=DAE6V_vZuFw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton")
+    })
+})
