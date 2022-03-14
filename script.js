03986f891f4dfd5e567be9b47c2c3cc3e1477495
kirby121 <83082132+kirby121@users.noreply.github.com>
2022-03-14 09:59:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ $trabajo1 = d.querySelector(".trabajo1")
 
 d.addEventListener("DOMContentLoaded", e =>{
     $trabajo1.addEventListener("click", e =>{
-        window.open("https://www.canva.com/design/DAE6V_vZuFw/eRZBS5X3ezIIDcSBlHQbEA/view?utm_content=DAE6V_vZuFw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton")
+        window.open("https://www.canva.com/design/DAE69o9DiYY/h3Tyh_EX7CygmUOF298uQQ/view?utm_content=DAE69o9DiYY&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink")
     })
 })
