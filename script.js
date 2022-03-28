108ae9055667a2cfe5b9e990cd9926910409c930
kirby121 <83082132+kirby121@users.noreply.github.com>
2022-03-28 17:03:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const d = document,
-$trabajo1 = d.querySelector(".trabajo1")
+$trabajo1 = d.querySelector(".trabajo1"),
+$trabajo2 = d.querySelector(".trabajo2")
 
 d.addEventListener("DOMContentLoaded", e =>{
-    $trabajo1.addEventListener("click", e =>{
-        window.open("https://www.canva.com/design/DAE69o9DiYY/h3Tyh_EX7CygmUOF298uQQ/view?utm_content=DAE69o9DiYY&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink")
-    })
+    $trabajo1.addEventListener("click", e => window.open("https://www.canva.com/design/DAE69o9DiYY/h3Tyh_EX7CygmUOF298uQQ/view?utm_content=DAE69o9DiYY&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"))
+    $trabajo2.addEventListener("click", e => window.open("https://www.canva.com/design/DAE8TbV9Oas/_k-27mhJ5bLNO-o2VHB0IQ/view?utm_content=DAE8TbV9Oas&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"))
 })
